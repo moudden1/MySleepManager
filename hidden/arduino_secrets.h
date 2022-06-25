e8435bb7e49dbe0cf6cfd0488803ca421a4a6bc6
sadistic001 <msadik@b2-IG-005PC05.domaine.lan>
2022-06-25 20:57:16 +0200

    added hidens files repo

---
+++ hidden/arduino_secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "iPhone de Mouad"
+#define SECRET_PASS "12345678"
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Otherwise leave the default "550" value. 
